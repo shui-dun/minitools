554d2ad36b15d70de840680f9abe34407634b355
shui-dun <2604697844@qq.com>
2021-03-13 11:39:38 +0800

    update

--- clash_helper/clash_helper.py
+++ clash_helper/clash_helper.py
@@ -9,7 +9,7 @@ lst = []
 
 def get_proxies_list():
     """
-    获取所有可用代理，存于lst中
+    获取所有可用代理，存于lst中,该函数会被switch_proxy调用，无需手动调用
     """
     global lst
     proxies = requests.get(url)
@@ -19,7 +19,7 @@ def get_proxies_list():
 
 def switch_proxy():
     """
-    切换使用的代理服务器
+    打开clash后调用该函数即可实现随机切换全局的代理
     """
     if len(lst) == 0:
         get_proxies_list()
--- headers2dict/headers2dict.py
+++ headers2dict/headers2dict.py
@@ -1,13 +1,12 @@
-import sys
+import pyperclip
+import json
 
 # 将http消息头转化为python字典
+# 从剪切板读入http消息头，将结果写入剪切板
 
 if __name__ == '__main__':
     d = dict()
-    with open(sys.argv[1]) as f:
-        for l in f.readlines():
-            pair = l.split(':', maxsplit=1)
-            d[pair[0]] = pair[1].strip()
-        print(d)
-    with open(sys.argv[1], 'w') as f:
-        f.write(str(d))
+    for l in pyperclip.paste().splitlines():
+        pair = l.split(':', maxsplit=1)
+        d[pair[0]] = pair[1].strip()
+    pyperclip.copy(json.dumps(d, indent=4))

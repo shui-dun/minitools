15e41efc622724d0a03ce32c04d9a919c688ea60
shui-dun <2604697844@qq.com>
2021-06-23 10:19:40 +0800

    update

--- bf2link/main.py
+++ bf2link/main.py
@@ -1,10 +1,10 @@
 import re
 import pyperclip
 
-# 将markdown中的粗体字变为链接
-# 例如，将 **hello** 变为 [填空](hello)
+# 将markdown中的粗体字变为链接，并将粗体的空格变为下划线
+# 例如，将 **hello world** 变为 [填空](hello_world)
 # 这样便于复习和考察
 if __name__ == '__main__':
     md = pyperclip.paste()
-    md = re.sub(r'\*\*(.+?)\*\*', lambda m: '[填空]({})'.format(m.group(1)), md)
+    md = re.sub(r'\*\*(.+?)\*\*', lambda m: '[填空]({})'.format(re.sub(r'\s+', '_', m.group(1))), md)
     pyperclip.copy(md)

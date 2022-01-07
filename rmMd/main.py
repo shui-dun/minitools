f51a1ca79a52914b2c10f1f226565f964033589a
shui-dun <2604697844@qq.com>
2022-01-07 20:18:01 +0800

    1. 添加到右键菜单 2. 将直接删除改为移动到回收站

--- rmMd/main.py
+++ rmMd/main.py
@@ -2,10 +2,12 @@ import pyperclip
 import re
 import os
 from urllib.parse import unquote
+import sys
+import send2trash
 
 if __name__ == '__main__':
-    # 从剪切板读取markdown文件的路径
-    mdPath = pyperclip.paste().strip('"')
+    # markdown文件的路径
+    mdPath = sys.argv[1]
     # 该markdown文件所引用的所有图片所在的所有文件夹
     imgFolders = set()
     # 该markdown文件所在的目录
@@ -19,16 +21,17 @@ if __name__ == '__main__':
     # 过滤到那些非本地的图片
     imgPathList = list(filter(lambda x: x[:4] != "http", imgPathList))
     for img in imgPathList:
-        imgFullPath = os.path.join(rootPath, img)
+        imgFullPath = os.path.join(rootPath, img).replace("/", "\\")
         # 如果图片路径有效
         if os.path.exists(imgFullPath):
             # 记录该图片所在的文件夹
             imgFolders.add(os.path.dirname(imgFullPath))
             # 删除该图片
-            os.remove(imgFullPath)
+            send2trash.send2trash(imgFullPath)
+            print(imgFullPath)
     # 删除在删除图片后变为空的文件夹
     for imgFolder in imgFolders:
         if len(os.listdir(imgFolder)) == 0:
-            os.removedirs(imgFolder)
+            send2trash.send2trash(imgFolder)
     # 删除Markdown文件本身
-    os.remove(mdPath)
+    send2trash.send2trash(mdPath)

f2ade2b912f7cd395d57e3cbdb2bd6aeafcc14c8
shui-dun <2604697844@qq.com>
2023-06-28 14:39:06 +0800

    完成autoWechat

--- autoWechat/README.md
+++ autoWechat/README.md
@@ -4,7 +4,34 @@
 
 ## 使用方法
 
+- 安装依赖：`pip install pyautogui pyperclip pygetwindow`
 - 按照 `msg.template.json` 编写 `msg.json`
 - 登录微信
 - 运行 `python main.py`
+
+## 打包方法
+
+如果目标电脑没有python环境，可以打包
+
+```python
+# 建立虚拟环境（如果不建立新环境，打包时会包含很多不需要的包）
+pip install pipenv
+pipenv install
+# 进入虚拟环境
+pipenv shell
+# 安装模块
+pip install pyautogui pyperclip pygetwindow
+# 打包的模块也要安装
+pip install pyinstaller
+# 设置编码，否则读取文件会有编码问题
+chcp 65001
+# 开始打包
+pyinstaller -Fw main.py
+```
+
+## 消息生成
+
+如果不知道该写什么消息，可以使用以下prompt询问chatGPT：
+
+> 请你扮演一个中国人，为我编写10条端午节祝福短信，简洁一点，口语化一些
 
---
+++ autoWechat/main.py
@@ -0,0 +1,39 @@
+import json
+import pyautogui as g
+import pyperclip
+import pygetwindow as gw
+import time
+
+if __name__ == '__main__':
+    # 根据窗口标题获取窗口
+    window = gw.getWindowsWithTitle("微信")[0]
+    # 如果窗口被最小化了，那么先恢复
+    if window.isMinimized:
+        window.restore()
+    # 将窗口置于最顶部
+    window.activate()
+
+    # 使用utf8编码打开json文件
+    with open('msg.json', 'r', encoding='utf8') as f:
+        msgs = json.load(f)
+    
+    pause = 0.5
+
+    for msgGroup in msgs:
+        appellations = msgGroup['appellation']
+        msg = msgGroup['msg']
+        for name, appellation in appellations.items():
+            finalMsg = f"{appellation}，{msg}"
+            g.hotkey('ctrl', 'f')
+            time.sleep(pause)
+            pyperclip.copy(name)
+            time.sleep(pause)
+            g.hotkey('ctrl', 'v')
+            time.sleep(pause)
+            g.hotkey('enter')
+            time.sleep(pause)
+            pyperclip.copy(finalMsg)
+            g.hotkey('ctrl', 'v')
+            time.sleep(pause)
+            g.hotkey('enter')
+            time.sleep(pause)

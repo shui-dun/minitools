6b77ec56f15cea2f719e46de04b4e1dc48fe385d
shui-dun <2604697844@qq.com>
2024-06-03 10:24:43 +0800

    简化输出

--- obsidian-calendar/calendar.js
+++ obsidian-calendar/calendar.js
@@ -1,11 +1,3 @@
-// loopWeeks: 例如[1, 3, 7]，表示每周1、周3、周7
-// loopMonths: 例如[1, 7, 31]，表示每月的1号、7号、31号
-// loopMonths2: 例如[[2, 6], [-2, 7]]，表示每月第2个周六和倒数第2个周日
-// loopYears: 例如[[1, 31], [3, 2]]，表示每年1月31日和3月2日
-// loopYears2: 例如[[4, 3, 1], [12, -1, 7]]，表示每年4月第3个星期一和12月最后一个星期日
-// startTime: 开始时分，例如12:00，如果为空表示全天事件
-// endTime: 结束时分，例如13:00
-
 let { DateTime } = dv.luxon;
 let today = DateTime.local().startOf("day");
 let oneWeekLater = today.plus({ days: 6 });
@@ -189,8 +181,6 @@ dv.table(
   ["", ""],
   events.map((e) => [
     e.title,
-    `<code>${e.date.toFormat("yyyy-MM-dd")} ${e.startTime || ""}->${
-      e.endTime || ""
-    }</code>`,
+    `<code>${e.date.toFormat("MM-dd") + ' '}${e.startTime || ""}${e.endTime ? '->' + e.endTime : ''}</code>`,
   ])
 );

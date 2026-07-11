614f81c9cfdfeeb01b7085a40f80d57ee5fb998f
shui-dun <2604697844@qq.com>
2026-07-11 10:26:38 +0800

    gamepad_mapper2

---
+++ gamepad_mapper/gamepad_mapper2.py
@@ -0,0 +1,38 @@
+# 该脚本通过手柄来触发键鼠按键以控制修改器
+# 该脚本必须用管理员身份运行
+from inputs import get_gamepad
+import win32api
+import win32con
+import time
+
+def press_keyboard():
+    # win32api.keybd_event(win32con.VK_MENU, 0, 0, 0)
+    # time.sleep(0.05)
+    win32api.keybd_event(win32con.VK_PAUSE, 0, 0, 0)
+    time.sleep(0.2)
+    win32api.keybd_event(win32con.VK_PAUSE, 0, win32con.KEYEVENTF_KEYUP, 0)
+    # time.sleep(0.05)
+    # win32api.keybd_event(win32con.VK_MENU, 0, win32con.KEYEVENTF_KEYUP, 0)
+
+def main():
+    gamepad_key1_pressed = False
+    gamepad_key2_pressed = False
+
+    while True:
+        try:
+            events = get_gamepad()
+            for event in events:
+                if event.code == 'ABS_HAT0Y':  # 十字键上下
+                    gamepad_key1_pressed = event.state == 1  # 1为下，-1为上，0为未按
+                if event.code == 'BTN_SOUTH':  # A键
+                    gamepad_key2_pressed = event.state == 1  # 1为按下，0为未按
+
+                # 检查是否同时按下
+                if gamepad_key1_pressed and gamepad_key2_pressed:
+                    press_keyboard()
+                    print("已发送键盘按键")
+                    gamepad_key1_pressed = False
+                    gamepad_key2_pressed = False
+                    break
+        except Exception as e: # 在手柄断开时仍能正常工作不退出
+            print(f"发生错误: {e}")
